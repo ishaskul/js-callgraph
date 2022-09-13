d05721a888c295545f570873a763941c8d8e0996
palex <pale@inf.u-szeged.hu>
2022-09-13 16:27:10 +0200

    Added tsx and jsx support

--- src/runner.js
+++ src/runner.js
@@ -222,7 +222,7 @@ exports.setFiles = function (inputList) {
             console.warn('The path "' + file + '" does not exists.');
         } else if (fs.statSync(file).isDirectory()) {
             filelist = utils.collectFiles(file, filelist);
-        } else if (file.endsWith(".js") || file.endsWith(".ts") || file.endsWith(".vue")) {
+        } else if (file.endsWith(".js") || file.endsWith(".jsx") || file.endsWith(".ts") || file.endsWith(".tsx") || file.endsWith(".vue")) {
             filelist.push(file);
         }
     });
